693fb288229fd61de48ea92933f9a1b7f20f5be8
jaysable82 <jaysable82@gmail.com>
2026-05-13 00:47:37 +0530

    added images

--- exp6-webview/app/src/main/java/com/example/exp_6/MainActivity.java
+++ exp6-webview/app/src/main/java/com/example/exp_6/MainActivity.java
@@ -9,17 +9,17 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     WebView webView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        webView = findViewById(R.id.webView);
+
+        webView=findViewById(R.id.webView);
 
         webView.setWebViewClient(new WebViewClient());
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.youtube.com");
 
-        webView.loadUrl("https://www.google.com");
     }
 }
